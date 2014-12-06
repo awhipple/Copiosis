a30b320cb0f74e381d69104145a089c05e48c5de
Kyle Galbraith <kyle.galbraith459@gmail.com>
2014-12-06 15:03:23 -0800

    Setting up the skeleton .NET application
    [#83794524]
    - Setting up the skeleton application with the Home and Account
    controllers. Also setup the initial actions to return some empty views.
    - NOTE: The db connection is not setup at all. Once we have a db setup,
    I will wire this up so we have the appropriate entities within the
    application.

---
+++ Copiosis Application/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+/* Home controller is used for pages viewed by a user that is not logged in */
+namespace Copiosis_Application.Controllers
+{
+    public class HomeController : Controller
+    {
+        /* The landing page will be a login area, could redirect to the Login action? */
+        public ActionResult Index()
+        {
+            return RedirectToAction("Login", "Account");
+            //return View();
+        }
+    }
+}
