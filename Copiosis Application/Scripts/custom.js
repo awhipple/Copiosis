ce44a634834924a8bd57a7f252bc3a317432ffef
Poolet <tpoole@pdx.edu>
2014-12-28 16:13:08 -0800

    Registration Page
    [#83792066] Basic layout of input forms

--- Copiosis Application/Scripts/custom.js
+++ Copiosis Application/Scripts/custom.js
@@ -7,44 +7,36 @@ function resizer() {
     var windowsize = $(window).width();
 
 
-    $('#navLoggedIn').toggleClass('text-right', (windowsize < 768));
-    $('#navLoggedIn').toggleClass('pull-right', (windowsize < 768));
+    $('#customNav').toggleClass('text-right', (windowsize < 1200));
+    $('#customNav').toggleClass('pull-right', (windowsize < 1200));
 
-    $('.navbar-text').toggleClass('text-right', (windowsize < 768));
-    $('.navbar-text').toggleClass('pull-right', (windowsize < 768));
+    $('.navbar-text').toggleClass('text-right', (windowsize < 1200));
+    $('.navbar-text').toggleClass('pull-right', (windowsize < 1200));
 
-    $('#navLoggedOut').toggleClass('text-right', (windowsize < 768));
-    $('#navLoggedOut').toggleClass('pull-right', (windowsize < 768));
+    $('#customNav').toggleClass('text-right', (windowsize >= 1200));
+    $('#customNav').toggleClass('pull-right', (windowsize >= 1200));
 
-    $('#navLoggedIn').toggleClass('text-right', (windowsize >= 768));
-    $('#navLoggedIn').toggleClass('pull-right', (windowsize >= 768));
+    $('.navbar-text').toggleClass('text-right', (windowsize >= 1200));
+    $('.navbar-text').toggleClass('pull-right', (windowsize >= 1200));
 
-    $('.navbar-text').toggleClass('text-right', (windowsize >= 768));
-    $('.navbar-text').toggleClass('pull-right', (windowsize >= 768));
-
-    $('#navLoggedOut').toggleClass('text-right', (windowsize >= 768));
-    $('#navLoggedOut').toggleClass('pull-right', (windowsize >= 768));
-
-    if (windowsize < 768) {
+    if (windowsize < 1200) {
         $('#brandHolder').css('text-align', 'center');
         $('#brandHolder').css('margin-bottom', '10px');
         $('.navbar-header').css('margin-top', '20px');
         $('.navbar-brand').css('float', 'center');
         $('#navUserHolder').removeAttr('style');
         $('.navbar-brand').addClass('text-center');
-        $('#navLoggedIn').addClass('text-center');
+        $('#customNav').addClass('text-center');
         $('.navbar-text').addClass('text-center');
-        $('#navLoggedOut').addClass('text-center');
     }
-    if (windowsize >= 768) {
+    if (windowsize >= 1200) {
         $('#brandHolder').removeAttr('style');
         $('.navbar-header').removeAttr('style');
         $('.navbar-brand').removeAttr('style');
         $('#navUserHolder').css('padding-right', '0');
         $('.navbar-brand').removeClass('text-center');
         $('.navbar-text').removeClass('text-center');
-        $('#navLoggedIn').removeClass('text-center');
-        $('#navLoggedOut').removeClass('text-center');
+        $('#customNav').removeClass('text-center');
     }
 
 };
