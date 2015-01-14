ddf1f3821e6f50bb79f07d7177681e8df8302425
Emerald Laurente <e.s.laurente@gmail.com>
2015-01-14 00:07:05 -0800

    Custom Error Page started
    [#85437358]
    Added changes to Error.cshtml to display a custom error page.
    
    Changed the appropriate line in <system.web> node of the Web.config file
    to allow this routing.
    
    Still need to make make the error messages more descriptive.
    
    Note: in the _Layout.cshtml file, I was getting build errors, so I had
    to wrap the script tags in the section blocks.

---
+++ Copiosis Application/Filters/ErrorHandlingAttribute.cs
@@ -0,0 +1,15 @@
+﻿//This is setup to be 
+using System;
+using System.Web.Mvc;
+
+namespace Copiosis_Application.Filters
+{
+    public class ErrorHandlingAttribute : FilterAttribute, IExceptionFilter
+    {
+        public void OnException(ExceptionContext filterContext)
+        {
+            filterContext.Result = new RedirectResult("~/Shared/Error.cshtml");
+            filterContext.ExceptionHandled = true;
+        }
+    }
+}
