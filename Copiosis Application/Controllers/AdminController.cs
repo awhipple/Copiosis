4c6b68016c3e104600e34865fbbf815a97ab7f0d
Michael Nishida <mnishida@pdx.edu>
2015-01-24 13:08:26 -0800

    Revert "Added AddClass And _ClassCRUDPartial views"
    This reverts commit 18a91dc221a087ed82e5731685373c8927bcf295.

--- Copiosis Application/Controllers/AdminController.cs
+++ Copiosis Application/Controllers/AdminController.cs
@@ -40,7 +40,6 @@ namespace Copiosis_Application.Controllers
         // GET: /Admin/AddClass
         // Add a new item class to Copiosis. Just returns the view.
         [HttpGet]
-        [AllowAnonymous]
         public ActionResult AddClass()
         {
             return View();
@@ -50,7 +49,6 @@ namespace Copiosis_Application.Controllers
         // POST: /Admin/AddClass
         // Add a new item class to Copiosis.
         [HttpPost]
-        [AllowAnonymous]
         public ActionResult AddClass(AddClassModel m)
         {
 
