b268151a84929fcf1d9a5db9371af347d7bd8203
awhipple <a.rgwhip@gmail.com>
2015-05-16 19:00:56 -0700

    Created web deploy version of the site

--- Copiosis Application/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Copiosis_Application.MvcApplication" Language="C#" %>
--- Copiosis.Data/Admin Scripts/Make_User_ADMIN.sql
+++
@@ -1,6 +0,0 @@
-/* Use this script to make a user have the ADMIN role which they need to
-   gain access to /Admin pages. 
-   values(UserID, 1), note: 1 is the admin role if you were to look at the
-   dbo.webpages_Roles table */
-insert into dbo.webpages_UsersInRoles
-values((select userID from [dbo].[user] where username = 'kylevc'), 1)
